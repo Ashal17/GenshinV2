90f93d7b6115f473fb88a2b24dab329532b7abf4
ashal17 <scheetah@seznam.cz>
2024-10-05 22:28:38 +0200

    init2

---
+++ equip.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>	
+    <title>Ashal - Genshin</title>
+
+<?php
+	require_once "header.php";
+?>
+</head>
+
+<body class="dark_theme">
+
+</body>
